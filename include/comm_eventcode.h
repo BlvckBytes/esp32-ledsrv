21e6ea5f47a0d67c735e52390b1c7500d7dd5335
Philip Hell <blvckbytes@gmail.com>
2022-02-01 16:31:24 +0100

    Formatted enums properly

--- include/comm_eventcode.h
+++ include/comm_eventcode.h
@@ -6,13 +6,15 @@
 
 // WARNING: Do NOT offset these, as they are used as array indices
 typedef enum {
-  FRAME_DUR_SET,
-  NUM_FRAMES_NUM_PIXELS_SET,
-  FRAME_CONT_SET,
-  BRIGHTNESS_SET,
-  WIFI_CRED_SET,
-  SD_CARD_STATE,
-  DEV_NAME_SET,
+  /*         NAME               |  VALUE  |    ARGS                 */
+
+  FRAME_DUR_SET                   = 0x0, //   <frame_dur uint16_t>
+  NUM_FRAMES_NUM_PIXELS_SET,             //   <num_frames uint16_t><num_pixels uint16_t>
+  FRAME_CONT_SET,                        //   <frame_index uint16_t><FRAME>
+  BRIGHTNESS_SET,                        //   <brightness uint8_t>
+  WIFI_CRED_SET,                         //   <ssid_bytes\0>
+  SD_CARD_STATE,                         //   <state_bool uint8_t>
+  DEV_NAME_SET,                          //   <dev_name_bytes\0>
 } CommEventCode;
 
 #endif
--- include/comm_opcode.h
+++ include/comm_opcode.h
@@ -4,19 +4,18 @@
 typedef enum {
   /*         NAME               |  VALUE  | SINGLE FRAME | R/W | ARGS                 */
 
-  SET_FRAME_DUR                 = 0x00, //      y          W    <uint_16t>
-  SET_NUM_FRAMES_NUM_PIXELS     = 0x01, //      y          W    <uint_16t><uint16_t>
-  SET_FRAME_CONT                = 0x02, //      n          W    <uint_16t><FRAME>
-  SET_BRIGHTNESS                = 0x03, //      y          W    <uint_8t>
-  SET_WIFI_CRED                 = 0x04, //      n          W    <ssid\0><pass\0>
-  SET_EVT_SUB                   = 0x05, //      y          W    <uint8_t><uint8_t>
-  SET_DEV_NAME                  = 0x06, //      n          W    <dev_name\0>
-  SET_NUM_PIXELS                = 0x07, //      y          W    <uint16_t>
+  SET_FRAME_DUR                 = 0x00, //      y          W    <frame_dur uint_16t>
+  SET_NUM_FRAMES_NUM_PIXELS     = 0x01, //      y          W    <num_frames uint_16t><num_pixels uint16_t>
+  SET_FRAME_CONT                = 0x02, //      n          W    <frame_index uint_16t><FRAME>
+  SET_BRIGHTNESS                = 0x03, //      y          W    <brightness uint_8t>
+  SET_WIFI_CRED                 = 0x04, //      n          W    <ssid_bytes\0><pass_bytes\0>
+  SET_EVT_SUB                   = 0x05, //      y          W    <event_index uint8_t><state_bool uint8_t>
+  SET_DEV_NAME                  = 0x06, //      n          W    <dev_name_bytes\0>
 
   GET_FRAME_DUR                 = 0x80, //      y          R    /
   GET_NUM_FRAMES_NUM_PIXELS     = 0x81, //      y          R    /
   GET_FRAME_SLOTS               = 0x82, //      y          R    /
-  GET_FRAME_CONT                = 0x83, //      n          R    <uint_16t>
+  GET_FRAME_CONT                = 0x83, //      n          R    <frame_index uint_16t>
   GET_BRIGHTNESS                = 0x84, //      y          R    /
   GET_WIFI_SSID                 = 0x85, //      y          R    /
   GET_SD_SIZE                   = 0x86, //      y          R    /
